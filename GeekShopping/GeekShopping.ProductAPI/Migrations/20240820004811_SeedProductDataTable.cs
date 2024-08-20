153667ceb182f9e9267393cf211bab00eb50807c
Erick Germani <erickgermani13@gmail.com>
2024-08-19 23:41:00 -0300

    Criando o microsserviço ProductAPI

---
+++ GeekShopping/GeekShopping.ProductAPI/Migrations/20240820004811_SeedProductDataTable.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeekShopping.ProductAPI.Migrations
+{
+    public partial class SeedProductDataTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "id", "category_name", "description", "image_url", "name", "price" },
+                values: new object[,]
+                {
+                    { 1L, "T-shirt", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.", "https://placehold.co/400x600", "Product #1", 69.9m },
+                    { 2L, "T-shirt", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.", "https://placehold.co/400x600", "Product #2", 69.9m },
+                    { 3L, "T-shirt", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.", "https://placehold.co/400x600", "Product #3", 69.9m },
+                    { 4L, "T-shirt", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.", "https://placehold.co/400x600", "Product #4", 69.9m },
+                    { 5L, "T-shirt", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.", "https://placehold.co/400x600", "Product #5", 69.9m },
+                    { 6L, "Shoes", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.", "https://placehold.co/400x600", "Product #6", 69.9m },
+                    { 7L, "Shoes", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.", "https://placehold.co/400x600", "Product #7", 69.9m },
+                    { 8L, "Shoes", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.", "https://placehold.co/400x600", "Product #8", 69.9m },
+                    { 9L, "Shoes", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.", "https://placehold.co/400x600", "Product #9", 69.9m },
+                    { 10L, "Shoes", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.", "https://placehold.co/400x600", "Product #10", 69.9m },
+                    { 11L, "Hat", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.", "https://placehold.co/400x600", "Product #11", 69.9m },
+                    { 12L, "Hat", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.", "https://placehold.co/400x600", "Product #12", 69.9m },
+                    { 13L, "Hat", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.", "https://placehold.co/400x600", "Product #13", 69.9m },
+                    { 14L, "Hat", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.", "https://placehold.co/400x600", "Product #14", 69.9m },
+                    { 15L, "Hat", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.", "https://placehold.co/400x600", "Product #15", 69.9m }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 3L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 4L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 5L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 6L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 7L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 8L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 9L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 10L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 11L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 12L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 13L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 14L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 15L);
+        }
+    }
+}
---
+++ GeekShopping/GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Model.Context
+{
+    public class MySQLContext : DbContext
+    {
+        public MySQLContext()
+        {
+
+        }
+
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            Dictionary<int, string> categories = new Dictionary<int, string>
+                {
+                    { 1, "T-shirt" },
+                    { 2, "Shoes" },
+                    { 3, "Hat" }
+                };
+
+            modelBuilder.Entity<Product>().HasData(
+                Enumerable.Range(1, 15).Select(i =>
+                {
+                    int categoriesIndex = (i - 1) / 5 + 1;
+
+                    return new Product
+                    {
+                        Id = i,
+                        Name = $"Product #{i}",
+                        Price = 69.9m,
+                        Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+                        CategoryName = categories[categoriesIndex],
+                        ImageUrl = "https://placehold.co/400x600"
+                    };
+                }).ToArray()
+            );
+        }
+
+    }
+}
